9877935ffde1129caf3ccc9d68f4106120f81525
Bilal Ali Syed <sayedbilalali614@gmail.com>
2020-02-05 11:57:41 +0530

    folder added

---
+++ Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using trackpuls.ViewModels;
+
+namespace trackpuls
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper() {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // base.OnStartup(sender, e);
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
