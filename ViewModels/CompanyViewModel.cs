c30b9f3577e26176e98c4d4604ed96184376ea0b
Bilal Ali Syed <sayedbilalali614@gmail.com>
2020-02-10 13:56:13 +0530

    Adding the TrackMain

--- ViewModels/CompanyViewModel.cs
+++ ViewModels/CompanyViewModel.cs
@@ -9,6 +9,21 @@ namespace trackpuls.ViewModels
 {
     public class CompanyViewModel : Screen
     {
+        public void btnSelectCompany() {
 
+            var conductor = this.Parent as IConductor;
+            conductor.ActivateItem(new TrackMainViewModel());
+        }
+        public void SayHello()
+        {
+            System.Windows.MessageBox.Show("Hello World From the Model");
+        }
+        public void SayHello(string name)
+        {
+           // System.Windows.MessageBox.Show("Hello World From the Model " + name);
+            var conductor = this.Parent as IConductor;
+            conductor.ActivateItem(new TrackMainViewModel());
+
+        }
     }
 }
---
+++ ViewModels/TrackMainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace trackpuls.ViewModels
+{
+    class TrackMainViewModel : Conductor<object>
+    {
+    }
+}
