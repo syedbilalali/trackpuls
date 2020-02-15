6546768569b69326eb4f4f81a61bdb497688ff94
Bilal Ali Syed <sayedbilalali614@gmail.com>
2020-02-15 21:25:13 +0530

    Insert the Screenshot Taker as background Process

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
 using System.IO;
+using System.ComponentModel;
 using System.IO.IsolatedStorage;
 using trackpuls.ViewModels;
+using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace trackpuls
 {
     class Bootstrapper : BootstrapperBase
     {
+        private DispatcherTimer timer;
         public Bootstrapper() {
             Initialize();
+            timer = new DispatcherTimer();
         }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
@@ -50,16 +56,20 @@ namespace trackpuls
                 throw;
             }
             DisplayRootViewFor<MainViewModel>();
+          
+            timer.Interval = TimeSpan.FromSeconds(60);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
         protected override void OnExit(object sender, EventArgs e)
-        {   
-           
+        {
+
             try
             {
-              
+
                 //First get the 'user-scoped' storage information location reference in the assembly
                 IsolatedStorageFile isolatedStorage = IsolatedStorageFile.GetUserStoreForAssembly();
-               
+
                 //create a stream writer object to write content in the location
                 StreamWriter srWriter = new StreamWriter(new IsolatedStorageFileStream("isotest", FileMode.Create, isolatedStorage));
                 System.Windows.MessageBox.Show(" On Exit " + srWriter.ToString());
@@ -72,8 +82,8 @@ namespace trackpuls
                     srWriter.WriteLine(App.Current.Properties["email"].ToString());
 
                 }
-               
 
+                timer.Stop();
                 srWriter.Flush();
                 srWriter.Close();
             }
@@ -82,6 +92,29 @@ namespace trackpuls
                 MessageBox.Show(sx.Message);
                 throw;
             }
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+
+           try { 
+            Bitmap captureBitmap = new Bitmap(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+                Random random = new Random();
+            //Creating a Rectangle object which will capture our Current Screen
+            System.Drawing.Rectangle captureRectangle = System.Windows.Forms.Screen.AllScreens[0].Bounds;
+
+            //Creating a New Graphics Object
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+            //Copying Image from The Screen
+            captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+
+            //Saving the Image File (I am here Saving it in My E drive).
+            captureBitmap.Save(@"D:\Screenshot\Capture_"+random.Next()+".jpg", ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception In Time Ticker Methods.. " + ex.Message);
+           }
         }
 
     }
